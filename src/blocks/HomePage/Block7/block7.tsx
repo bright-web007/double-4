0df46492b0994ab86d03052c3a8e57358816c23a
Bright Williams <brightwilliams22222@gmail.com>
2025-09-10 17:52:41 +0100

    update

--- src/blocks/HomePage/Block7/block7.tsx
+++ src/blocks/HomePage/Block7/block7.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import Image from "next/image";
 
+type OpeningHour = {
+    day: string;
+    time: string;
+  };
 const block7 = () => {
-    const hours = [
+    const hours: OpeningHour[] = [
         { day: "Monday", time: "CLOSED" },
         { day: "Tuesday", time: "11AM - 9PM" },
         { day: "Wednesday", time: "11AM - 9PM" },
